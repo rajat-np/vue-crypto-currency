ed04a352de6e8faf802ad3f1d5d9998535729548
sengarsameer <sengar.sameer@gmail.com>
2021-02-12 13:14:57 +0530

    add header component

---
+++ src/components/header/HeaderBar.vue
@@ -0,0 +1,19 @@
+<template> 
+    <h1 class="p-3 text-center">
+        {{title}}
+    </h1>
+</template>
+
+<script>
+export default {
+    name: 'HeaderBar',
+    props: ['headerTitle'],
+    data() {
+        return {
+            title: this.headerTitle
+        }
+    }
+};
+</script>
+
+<style></style>
