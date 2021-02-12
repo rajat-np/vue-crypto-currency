46012ef0eca3d424cd812d1ae36cf5bb436a0978
sengarsameer <sengar.sameer@gmail.com>
2021-02-12 13:15:52 +0530

    add pager component

---
+++ src/components/pager/PageSize.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- <b-col sm="5" md="6" class="my-1"> -->
+    <b-form-group
+        label="Per page"
+        label-for="per-page-select"
+        label-cols-md="10"
+        label-align-sm="right"
+        label-size="sm"
+        class="mb-3"
+    >
+        <b-form-select
+            id="per-page-select"
+            v-model="perPage"
+            :options="pageOptions"
+            size="sm"
+            align="right"
+        ></b-form-select>
+    </b-form-group>
+    <!-- </b-col> -->
+</template>
+
+<script>
+export default {
+    name: "PageSize",
+    props: ["pageOptions", "perPage"],
+    date() {
+        return {};
+    },
+};
+</script>
+
+<style></style>
